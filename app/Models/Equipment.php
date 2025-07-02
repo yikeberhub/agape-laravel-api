21ba2eead81087e4eba4fe70a144c3b3fbedc13e
Yikeber <yikeber50@gmail.com>
2025-07-02 23:23:12 +0300

    disability model created

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -1,5 +1,3 @@
-<?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 class Equipment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'equipment_type',
+        'size',
+        'cause_of_need',
+    ];
+
+    public function disabilityRecords()
+    {
+        return $this->hasMany(Disability::class);
+    }
 }
