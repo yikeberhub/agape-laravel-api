6b9fa626ec8f772499f0855a81f8f7181ed233aa
Yikeber <yikeber50@gmail.com>
2025-07-02 22:31:47 +0300

    users migration implemented

--- database/migrations/2025_07_02_191838_create_users_table.php
+++ database/migrations/2025_07_02_191838_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -15,6 +16,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('email');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->string('phone_number')->nullable()->unique();
+            $table->string('profile_image')->nullable();
+            $table->enum('role',['admin','field_worker'])->default('field_worker');
+            $table->boolean('is_active')->default(false);
+            $table->boolean('is_deleted')->default(false);
             $table->timestamps();
         });
     }
