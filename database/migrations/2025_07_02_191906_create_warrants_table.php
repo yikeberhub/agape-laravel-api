6c89fe7482cf50038b63e27aa10b4fd50415396b
Yikeber <yikeber50@gmail.com>
2025-07-02 22:37:18 +0300

    warrant migration table created

--- database/migrations/2025_07_02_191906_create_warrants_table.php
+++ database/migrations/2025_07_02_191906_create_warrants_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('warrants', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->string('phone_number')->nullable()->unique();
+            $table->string('id_image')->nullable();
+            $table->boolean('is_deleted')->default(false);
             $table->timestamps();
         });
     }
