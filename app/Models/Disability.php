c00bc8889d706644b03079ee824f18c5e1caa1cf
Yikeber <yikeber50@gmail.com>
2025-07-02 23:26:31 +0300

    disability record model is created

--- app/Models/Disability.php
+++ app/Models/Disability.php
@@ -1,5 +1,3 @@
-<?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +6,43 @@ use Illuminate\Database\Eloquent\Model;
 class Disability extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'gender',
+        'date_of_birth',
+        'phone_number',
+        'region',
+        'zone',
+        'city',
+        'woreda',
+        'recorder_id',
+        'warrant_id',
+        'equipment_id',
+        'hip_width',
+        'backrest_height',
+        'thigh_length',
+        'profile_image',
+        'id_image',
+        'is_provided',
+        'is_active',
+        'is_deleted',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'recorder_id');
+    }
+
+    public function warrant()
+    {
+        return $this->belongsTo(Warrant::class, 'warrant_id');
+    }
+
+    public function equipment()
+    {
+        return $this->belongsTo(Equipment::class, 'equipment_id');
+    }
 }
