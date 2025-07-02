611783e691cbf3659eff7d687e465039ecd23274
Yikeber <yikeber50@gmail.com>
2025-07-02 23:19:57 +0300

    warrent model detail created

--- app/Models/Warrant.php
+++ app/Models/Warrant.php
@@ -1,5 +1,3 @@
-<?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 class Warrant extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'phone_number',
+        'gender',
+        'id_image',
+        'is_deleted',
+    ];
+
+    public function disabilityRecords()
+    {
+        return $this->hasMany(Disability::class);
+    }
 }
